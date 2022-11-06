958c4721e5c6932ffda3ccccdb8a5879a922af2b
Maxim Shyryaev <ipz223_shmye@student.ztu.edu.ua>
2022-11-06 15:08:52 +0300

    Punkt 28

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Повідомлення 1");
-Console.WriteLine("Повідомлення 2");
-Console.WriteLine("Повідомлення 3");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.ReadKey();
